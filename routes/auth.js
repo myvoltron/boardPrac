09f706e52131b7408641a6cd2a862aeb1b108f24
myvoltron <leejs8266@gmail.com>
2022-01-01 07:10:12 +0900

    login logout work in auth.js

---
+++ routes/auth.js
@@ -0,0 +1,30 @@
+// 회원가입 및 로그인 처리
+const express = require('express');
+const passport = require('passport'); 
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.redirect('/auth/login');
+});
+
+// 로그인 form 
+router.get('/login', (req, res) => {
+    const user = req.user; 
+    console.log(user); 
+    res.render('login', { user }); 
+}); 
+
+router.post('/login', passport.authenticate('local', {
+    failureRedirect: '/auth/login', 
+}), (req, res) => { 
+    res.redirect('/board'); 
+});
+
+// 로그아웃
+router.get('/logout', (req, res) => {
+    console.log('로그아웃 합니다'); 
+    req.logout(); // passport log out
+    res.redirect('/auth/login'); 
+});
+
+module.exports = router; 
